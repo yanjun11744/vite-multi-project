acd07103634233f7434e0499c36ea3ee6ea58ec8
yanjun11744 <sunyanjun11744@163.com>
2024-08-18 09:55:38 +0800

    忽略模板路由错误提示

--- scripts/template-ts/router/routes.ts
+++ scripts/template-ts/router/routes.ts
@@ -1,7 +1,7 @@
 import { RouteRecordRaw } from 'vue-router'
 export const routes: Array<RouteRecordRaw> = [
   {
-    path: '/home',
+    path: '/',
     name: 'index',
     //@ts-ignore
     component: () => import('@projects/页面名称/views/index.vue'),
